951990de34551f9687cf646048549f1949c1776e
JECONIAH23 <110761117+JECONIAH23@users.noreply.github.com>
2022-09-14 14:28:56 +0300

    modified donations,html

--- pay.php
+++ pay.php
@@ -1,19 +1,34 @@
+
+
 <?php
-    $SERVERNAME="localhost";
-    $USERNAME="root";
-    $PASSWORD="";
-    $DBNAME="green";
-    $conn=mysqli_connect($SERVERNAME,$USERNAME,$PASSWORD,$DBNAME);
 
-    if(isset($_POST['submit'])){
-        $name=$_POST['name'];
-        $amount=$_POST['amount'];
-        $number=$_POST['mobile']
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+try{
+    $pdo = new PDO("mysql:host=localhost;dbname=gogreen", "root", "");
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e){
+    die("ERROR: Could not connect. " . $e->getMessage());
+}
+ 
+// Read from form
+if(isset($_POST['submit'])){
+    $name=$_POST['name'];
+    $amount=$_POST['amount'];
+    $number=$_POST['mobile']
 
-        $pay = "INSERT INTO transact ( `mobile`, `amount`, `user`) VALUES ( '$mobile', '$amount', '$name')";
-        
-        if($patients = mysqli_query($conn,$pay) ){
-            header("location:pages/index.html");
-        }
-         }
-?>
+    $sql = "INSERT INTO transact (mobile,amount,user) VALUES ($number, $amount, $name)"; 
+
+// Attempt insert query execution
+// try{
+       
+//     $pdo->exec($sql);
+//     echo "Records inserted successfully.";
+// // } catch(PDOException $e){
+// //     die("ERROR: Could not able to execute $sql. " . $e->getMessage());
+// // }
+ 
+// // Close connection
+// unset($pdo);
+// ?>
